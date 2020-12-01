4ae8822ee46d16e50b90b182f816b85c7c5e3eff
Emu <e.greshkov@student.tue.nl>
2020-12-01 12:30:33 +0100

    Day one

---
+++ Day1/Day1.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+///converts a string to an integer
+///assumes that all characters will be of integers
+int strToInt(std::string s){
+    int answer = 0;
+    for(int i = 0; i < s.size(); i++){
+        answer = answer*10 + s[i] - '0';
+    }
+    return answer;
+}
+
+///finds 2 numbers that sum to 2020 and returns their product
+int find2020_2(std::vector<int> v){
+    int sz = v.size();
+    for(int i = 0; i < sz-1; i++){
+        ///std::cout<<nums[i]<<'\n';
+        for(int j = 0; j <sz; j++){
+            if(v[i] + v[j] == 2020){
+                return v[i] * v[j];
+            }
+        }
+    }
+    return 0;
+}
+
+///finds 3 numbers that sum to 2020 and returns their product
+int find2020_3(std::vector<int> v){
+    int sz = v.size();
+
+    for(int i = 0; i < sz - 2; i++){
+        for(int j = i + 1; j <sz - 1; j++){
+            for(int k = j + 1; k < sz; k++){
+                if(v[i] + v[j] + v[k] == 2020){
+                    return v[i] * v[j] * v[k];
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    ///initialize file connections
+    std::ifstream iFile ("input.txt");
+    std::ofstream oFile ("output.txt");
+
+    std::string line;///read each line
+    std::vector<int>nums;///store the numbers
+    if (iFile.is_open()){
+        while ( getline (iFile,line) ){
+            nums.push_back(strToInt(line));
+        }
+    }
+
+    int ans = find2020_2(nums);
+    oFile << ans <<'\n';
+
+    ans = find2020_3(nums);
+    oFile << ans <<'\n';
+
+    iFile.close();
+    oFile.close();
+    return 0;
+}
