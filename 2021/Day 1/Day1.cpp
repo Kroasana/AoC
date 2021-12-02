19d3be50215256733bde157de0b1f7e9c53ee709
Emu <e.greshkov@student.tue.nl>
2021-12-02 08:57:06 +0100

    Update Day 1 solution to something more neat

--- 2021/Day 1/Day1.cpp
+++ 2021/Day 1/Day1.cpp
@@ -36,28 +36,22 @@ int main(){
     int prevNum = INT_MAX;
     int currNum = 0;
     std::string line;///read each line
-    std::vector<int>nums;///store the numbers
+    std::vector<int>sums;///store the numbers
     int lineCount=0;
     if (iFile.is_open()){
+        sums.push_back(0);
         while ( getline (iFile,line) ){
-            nums.push_back(strToInt(line));
+            sums.push_back(sums[sums.size()-1]+strToInt(line));
         }
     }
-    std::vector<int>sums;
-    sums.push_back(nums[0]+nums[1]+nums[2]);
-    sums.push_back(nums[1]+nums[2]);
-    sums.push_back(nums[2]);
-    for(int i = 1; i < nums.size(); i++){
-        if(nums[i]>nums[i-1]){
+    for(int i = 2; i < sums.size(); i++){
+        if(sums[i] - sums[i-1] > sums[i-1]-sums[i-2]){
             answer1++;
         }
-        if(i > 2){
-            sums[sums.size() - 1] += nums[i];
-            sums[sums.size() - 2] += nums[i];
-            if(sums[sums.size() - 2] > sums[sums.size() - 3]){
+        if(i > 3){
+            if(sums[i]-sums[i-3] > sums[i-1] - sums[i-4]){
                 answer2++;
             }
-            sums.push_back(nums[i]);
         }
     }
     std::cout<<answer1<<' '<<answer2<<'\n';
