27a79f7441c71e6ac01a3830ed3a2655a00dfb6e
Emiliyan Greshkov <greshkat@abv.bg>
2023-12-08 12:40:38 +0200

    [2023-cpp] Day 8

--- 2023/Day 8/Day8.cpp
+++ 2023/Day 8/Day8.cpp
@@ -87,6 +87,16 @@ std::vector<std::string> split(const std::string &s, char delim) {
     return result;
 }
 
+long long gcd(long long int a, long long int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) {
+    return (a / gcd(a, b)) * b;
+}
+
 template <typename TI, typename T>
 std::vector<T> mapVector(std::vector<TI> input, std::function<T(TI)> func) {
     std::vector<T> result;
@@ -100,20 +110,72 @@ std::vector<T> mapVector(std::vector<TI> input, std::function<T(TI)> func) {
 
 int main() {
     // initialize file connections
-    std::ifstream iFile("testinput.txt");
+    std::ifstream iFile("input.txt");
     std::ofstream oFile("output.txt");
 
     auto tStart = std::chrono::high_resolution_clock::now();
 
-    int answer1 = 0, answer2 = 0;
+    long long answer1 = 0, answer2 = 0;
     std::string line;
-
+    std::unordered_map<std::string, std::pair<std::string, std::string>> map;
+    std::queue<char> queue;
+    std::queue<char> queue2;
     if (iFile.is_open()) {
         // Read the input file line by line.
+        getline(iFile, line);
+        for (char c : line) {
+            queue.push(c);
+        }
+        queue2 = queue;
+        getline(iFile, line);
+
+        std::vector<std::string> current2;
         while (getline(iFile, line)) {
-            // Do stuff with line
+            std::string key = split(line, '=')[0];
+            if (key[2] == 'A')
+                current2.push_back(key);
+            std::string left = split(split(split(line, '=')[1], ',')[0], '(')[0];
+            std::string right = split(split(split(line, '=')[1], ',')[1], ')')[0];
+            map[key] = {left, right};
+        }
+
+        std::string current = "AAA";
+        while (current != "ZZZ") {
+            char step = queue.front();
+            queue.pop();
+            queue.push(step);
+            if (step == 'L') {
+                current = map[current].first;
+            } else {
+                current = map[current].second;
+            }
+            answer1++;
+        }
+        bool allEndWithZ = false;
+        std::vector<long long> individualAnswers;
+        for (int thread = 0; thread < current2.size(); thread++) {
+            std::queue<char> lq = queue2;  // local queue
+            current = current2[thread];
+            long long currAns = 0;
+            while (current[2] != 'Z') {
+                char step = lq.front();
+                lq.pop();
+                lq.push(step);
+                if (step == 'L') {
+                    current = map[current].first;
+                } else {
+                    current = map[current].second;
+                }
+                currAns++;
+            }
+            individualAnswers.push_back(currAns);
+        }
+        answer2 = individualAnswers[0];
+        for (int i = 1; i < individualAnswers.size(); i++) {
+            answer2 = lcm(answer2, individualAnswers[i]);
         }
     }
+
     // Output to the console/terminal
     std::cout << answer1 << ' ' << answer2 << '\n';
     // Output to output.txt
