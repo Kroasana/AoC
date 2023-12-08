0e61e1636251811c362a7a597963a39157720fd1
Emiliyan Greshkov <greshkat@abv.bg>
2023-12-08 17:43:31 +0200

    [2023-cpp] Extract commonly used util functions into separate header.

--- 2023/Day8/Day8.cpp
+++ 2023/Day8/Day8.cpp
@@ -1,91 +1,10 @@
-#include <algorithm>
-#include <bitset>
 #include <chrono>
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <map>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <string>
 #include <unordered_map>
-#include <vector>
 
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-                return !std::isspace(ch);
-            }));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-                return !std::isspace(ch);
-            }).base(),
-            s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-    rtrim(s);
-    ltrim(s);
-}
-
-/// @brief Utility function to check whether a character is a digit.
-/// @param c The character to check.
-/// @return true if c is a digit character, false otherwise.
-bool isDigit(char c) {
-    return c >= '0' && c <= '9';
-}
-
-/// @brief Utility function to convert a char to a digit.
-/// @param c The character to convert.
-/// @return -1 if c is not between '0' and '9', the respective digit as an int otherwise.
-int charToInt(char c) {
-    if (!isDigit(c)) return -1;
-    return c - '0';
-}
-
-/// @brief Utility function to transform a string to a 32-bit integer
-/// @param s The string to transform.
-/// @return The integer value of the input string of decimal characters.
-int strToInt(std::string s) {
-    int answer = 0;
-    for (int i = 0; i < s.size(); i++) {
-        answer = answer * 10 + s[i] - '0';
-    }
-    return answer;
-}
-
-/// @brief Utility function to transform a string to a 64-bit integer.
-/// @param s The string to transform.
-/// @return The long long int value of the input string of decimal characters.
-long long strToLL(std::string s) {
-    long long answer = 0;
-    for (int i = 0; i < s.size(); i++) {
-        answer = answer * 10 + s[i] - '0';
-    }
-    return answer;
-}
-
-/// @brief Utility function to split a strings by a delimiter.
-/// @param s The string of words to split up.
-/// @param delim The delimiter by which to split the string.
-/// @return A vector strings, containing the separated substrings.
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> result;
-    std::stringstream ss(s);
-    std::string substring;
-
-    while (getline(ss, substring, delim)) {
-        trim(substring);
-        if (substring.size())
-            result.push_back(substring);
-    }
-    return result;
-}
+#include "../Utils.h"
 
 long long gcd(long long int a, long long int b) {
     if (b == 0)
@@ -95,15 +14,6 @@ long long gcd(long long int a, long long int b) {
 
 long long lcm(long long a, long long b) {
     return (a / gcd(a, b)) * b;
-}
-
-template <typename TI, typename T>
-std::vector<T> mapVector(std::vector<TI> input, std::function<T(TI)> func) {
-    std::vector<T> result;
-    for (TI inputElement : input) {
-        result.push_back(func(inputElement));
-    }
-    return result;
 }
 
 // TODO: Export the functions to a utils header
